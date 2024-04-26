caee3bb1bf674e6da6bc16464bc13004d9ffa183
devsjc <47188100+devsjc@users.noreply.github.com>
2024-04-26 09:03:37 +0100

    feat(plugin): Enable singleton downloading

--- beetsplug/ytdlp.py
+++ beetsplug/ytdlp.py
@@ -24,6 +24,15 @@ class AlbumDetails:
     def __str__(self):
         return f'{self.title} by {self.artist}'
 
+@dataclasses.dataclass
+class SingletonDetails:
+    title: str
+    artist: str
+    track_url: str
+
+    def __str__(self):
+        return f'{self.title} by {self.artist}'
+
 class YTDLPPlugin(BeetsPlugin):
     """A plugin for downloading music from YouTube and importing into beets."""
 
@@ -58,17 +67,37 @@ class YTDLPPlugin(BeetsPlugin):
             if self.config.get("verbose"):
                 print(f"[ytdlp] Running ytdlp with opts: {opts}")
 
-            album_details = self._get_album_details(opts.artist, opts.album, opts.url)
-            if not album_details:
+            if opts.track and opts.album:
+                print(f"{Colors.WARNING}[ytdlp] Cannot specify both track and album{Colors.END}")
+                return
+            
+            if opts.track:
+                track_details = self._get_track_details(opts.artist, opts.track, opts.url)
+                if not track_details:
+                    return
+
+                track_dir = self._download_singleton(track_details)
+                if not track_dir:
+                    return
+
+                self._import_singleton(track_dir)
+
+                print(f"{Colors.SUCCESS}[ytdlp] Successfully imported {track_details}{Colors.END}")
                 return
 
-            album_dir = self._download_album(album_details)
-            if not album_dir:
+            if opts.album:
+                album_details = self._get_album_details(opts.artist, opts.album, opts.url)
+                if not album_details:
+                    return
+
+                album_dir = self._download_album(album_details)
+                if not album_dir:
+                    return
+
+                self._import_album(album_dir)
+
+                print(f"{Colors.SUCCESS}[ytdlp] Successfully imported {album_details}{Colors.END}")
                 return
-
-            self._import_album(album_dir)
-
-            print(f"{Colors.SUCCESS}[ytdlp] Successfully imported {album_details}{Colors.END}")
 
         ytdlp_command = ui.Subcommand(
             'ytdlp',
@@ -93,6 +122,11 @@ class YTDLPPlugin(BeetsPlugin):
             help="Name of album",
         )
         parser.add_option(
+            "--track",
+            action="store",
+            help="Name of track",
+        )
+        parser.add_option(
             "-u", "--url",
             dest="url",
             action="store",
@@ -106,6 +140,29 @@ class YTDLPPlugin(BeetsPlugin):
         )
 
         return parser 
+
+    def _get_track_details(self, artist: str, track: str, url: str | None) -> SingletonDetails | None:
+        """Get details for track on YouTube."""
+        # If the url has been passed in, use that
+        if url:
+            return SingletonDetails(title=track, artist=artist, track_url=url)
+
+        # Otherwise perform a search via YTMusic api
+        if self.config.get('verbose'):
+            print(f'[ytdlp] Searching for {artist} - {track} on YouTube Music')
+        ytmusic = YTMusic()
+        search_results: list[dict] = ytmusic.search(f'{artist} {track}', filter="songs")
+        if not search_results:
+            print(f'[ytdlp] No results found for {artist} - {track}')
+            print('[ytdlp] Please check the artist and track names and try again.')
+            print('[ytdlp] Or consider passing in the url to a track via the --url flag.')
+            return None
+        song_details: dict = search_results[0]
+        return SingletonDetails(
+            title=song_details['title'],
+            artist=song_details['artists'][0]['name'],
+            track_url="https://youtube.com/watch?v=" + song_details['videoId']
+        )
 
     def _get_album_details(self, artist: str, album: str, url: str | None) -> AlbumDetails | None:
         """Get details for album playlist on YouTube."""
@@ -159,6 +216,30 @@ class YTDLPPlugin(BeetsPlugin):
 
         return self.cache_dir + "/" + ad.artist + "/" + ad.title
 
+    def _download_singleton(self, sd: SingletonDetails) -> str:
+        """Download track from YouTube."""
+        if self.config.get('verbose'):
+            print(f'[ytdlp] Downloading {sd}')
+
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'extractaudio': True,
+            'outtmpl': self.cache_dir + "/" + sd.artist + "/%(title)s.%(ext)s",
+            'postprocessors': [
+                {'key': 'FFmpegExtractAudio'},
+                {'key': 'FFmpegMetadata'},
+            ],
+        }
+
+        with YoutubeDL(ydl_opts) as ydl:
+            returncode = ydl.download([sd.track_url])
+
+        if returncode != 0:
+            print(f'[ytdlp] Error downloading {sd}')
+            return ""
+
+        return self.cache_dir + "/" + sd.artist
+
     def _import_album(self, album_dir: str) -> str | None:
         """Import album into beets."""
         beet_cmd = ['beet', 'import', '-m', album_dir]
@@ -173,8 +254,22 @@ class YTDLPPlugin(BeetsPlugin):
 
         return album_dir
 
-    def _clear_cache(self, album_dir: str) -> None:
-        """Clear the cache of downloaded albums."""
-        os.remove(album_dir)
+    def _import_singleton(self, track_dir: str) -> str | None:
+        """Import track into beets."""
+        beet_cmd = ['beet', 'import', '-m', '-s', track_dir]
+        if os.getenv('BEETS_ENV') == 'develop':
+            beet_cmd.extend(['-c', 'env.config.yml'])
+        if self.config.get('verbose'):
+            print("[ytdlp] Running beets: " + ' '.join(beet_cmd))
+        process = subprocess.run(beet_cmd)
+        if process.returncode != 0:
+            print(f"[ytdlp] Error importing {track_dir} into beets")
+            return None
+
+        return track_dir
+
+    def _clear_cache(self, d: str) -> None:
+        """Clear the cache of downloaded files."""
+        os.remove(d)
         
 
